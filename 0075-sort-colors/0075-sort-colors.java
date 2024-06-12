5f3b67e61bcde16e9ac46612f878bf426e2d4dc1
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-06-12 12:03:56 +0530

    Time: 1 ms (21.51%), Space: 41.3 MB (97.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,24 @@
 class Solution {
-    public void sortColors(int[] nums) {
+    static void Swap(int nums[] , int i , int j ){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
 
-        Arrays.sort(nums);
+    public void sortColors(int[] nums) {
+        int i , j,small;
+
+        for( i=0;i<nums.length-1;i++){
+            small=i;
+            for( j=i;j<nums.length;j++){
+                if(nums[j]<nums[small]){
+                    small=j;
+                }
+            }
+            Swap(nums,i,small);
+        }
+
+           
        
     }
 }
