1ff5e006d550cf38d085c956a1136e23ecad32a0
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-06-14 14:22:06 +0530

    Time: 37 ms (72.13%), Space: 59.9 MB (64.26%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -6,7 +6,7 @@ class Solution {
         int c = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] <= nums[i - 1]) {
-                c += nums[i - 1] + 1 - nums[i];
+                c += nums[i-1]  - nums[i] +1;
                 nums[i] = nums[i - 1] + 1;
             }
         }
