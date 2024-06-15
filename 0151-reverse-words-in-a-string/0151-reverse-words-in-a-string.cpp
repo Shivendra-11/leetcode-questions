f14f733f43f608abc57ad99a9e917469d4e6bd5f
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-06-15 17:16:22 +0530

    Time: 3 ms (81.76%), Space: 8.4 MB (78.52%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
-    string reverseWords(string s) {
-        // Reverse the entire string
-        reverse(s.begin(), s.end());
+    string reverseWords(string s) 
+    {
+        reverse(s.begin() , s.end());
+         int n=s.length();
+    int i=0;
+    int l=0;
+    int r=0;
 
-        int n = s.length();
-        int i = 0;
-        int l = 0;
-        int r = 0;
-
-        while (i < n) {
-            // Skip leading spaces
-            while (i < n && s[i] == ' ') {
-                i++;
-            }
-            // Mark the start of the word
-            l = r;
-            // Copy the word
-            while (i < n && s[i] != ' ') {
-                s[r++] = s[i++];
-            }
-            // Reverse the word
-            if (l < r) {
-                reverse(s.begin() + l, s.begin() + r);
-                s[r++] = ' ';
-            }
+    while(i<n){
+        while(i<n && s[i] != ' '){
+            s[r++]=s[i++];
         }
+        if(l<r){
+             reverse(s.begin() + l, s.begin() + r);
+            s[r]= ' ';
+            r++;
 
-        // Remove the trailing space if there is one
-        if (r > 0 && s[r - 1] == ' ') {
-            r--;
+            l=r;
         }
-
-        return s.substr(0, r);
+        i++;
     }
+    s= s.substr(0,r-1);
+    return s;
+    
+}
+
+        
+
 };
