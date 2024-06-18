857cb5527530808466fe20788403b764097acb48
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-06-18 18:44:27 +0530

    Time: 28 ms (50.65%), Space: 54.7 MB (86.69%) - LeetHub

---
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int M = 1_000_000_007;
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums.length - 1;
+        int result = 0;
+
+        int[] powerOfTwo = new int[nums.length];
+        powerOfTwo[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            powerOfTwo[i] = (powerOfTwo[i - 1] * 2) % M;
+        }
+
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                result = (result + powerOfTwo[r - l]) % M;
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return result;
+    }
+}
