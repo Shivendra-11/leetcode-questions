c8d68a1c55a799e5975d76f9082d2a77e46a0b03
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-06-21 02:00:49 +0530

    Time: 1 ms (76.35%), Space: 40.5 MB (86.38%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverse(int x) {
+        long rem=0;
+        while(x!=0){
+            int mod=x %10;
+            rem=rem * 10 +mod;
+            x/=10;
+        }
+        return (rem<Integer.MIN_VALUE || rem>Integer.MAX_VALUE)?0:(int) rem;
+    }
+}
