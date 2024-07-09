eea2619250720b8bd0613033256c76531346dc23
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-07-10 01:37:19 +0530

    Time: 26 ms (17.82%), Space: 47.2 MB (58.62%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -2,26 +2,26 @@ import java.util.HashMap;
 
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        HashMap<Integer, Integer> prefix = new HashMap<>();
-        prefix.put(0, 1); // Initialize with 0 to count subarrays starting from index 0
+            HashMap<Integer, Integer> prefix = new HashMap<>();
+                    prefix.put(0, 1); // Initialize with 0 to count subarrays starting from index 0
 
-        int sum = 0;
-        int count = 0;
+                            int sum = 0;
+                                    int count = 0;
 
-        for (int num : nums) {
-            sum += num;
-            int rem = sum % k;
-            if (rem < 0) {
-                rem += k;
-            }
+                                            for (int num : nums) {
+                                                        sum += num;
+                                                                    int rem = sum % k;
+                                                                                if (rem < 0) {
+                                                                                                rem += k;
+                                                                                                            }
 
-            if (prefix.containsKey(rem)) {
-                count += prefix.get(rem);
-            }
+                                                                                                                        if (prefix.containsKey(rem)) {
+                                                                                                                                        count += prefix.get(rem);
+                                                                                                                                                    }
 
-            prefix.put(rem, prefix.getOrDefault(rem, 0) + 1);
-        }
+                                                                                                                                                                prefix.put(rem, prefix.getOrDefault(rem, 0) + 1);
+                                                                                                                                                                        }
 
-        return count;
-    }
-}
+                                                                                                                                                                                return count;
+                                                                                                                                                                                    }
+                                                                                                                                                                                    }
