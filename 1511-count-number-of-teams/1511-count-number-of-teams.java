aea04a7e61841d962965b189c6d3de20ebed647e
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-07-29 19:11:39 +0530

    Time: 16 ms (82.28%), Space: 42.4 MB (52.18%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,33 +1,37 @@
 class Solution {
     public int numTeams(int[] nums) {
-        int c = 0;
-        
-        // Loop to count increasing sequences
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] < nums[j]) {
-                    for (int k = j + 1; k < nums.length; k++) {
-                        if (nums[j] < nums[k]) {
-                            c++;
-                        }
-                    }
+        int n = nums.length;
+        int count = 0;
+                                     
+        for (int i = 0; i < n; i++) {
+            int leftSmaller = 0, leftLarger = 0;
+            int rightSmaller = 0, rightLarger = 0;
+
+            // Count elements smaller and larger to the left of nums[i]
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    leftSmaller++;
+                } else if (nums[j] > nums[i]) {
+                    leftLarger++;
                 }
             }
-        }
-        
-        // Loop to count decreasing sequences
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] > nums[j]) {
-                    for (int k = j + 1; k < nums.length; k++) {
-                        if (nums[j] > nums[k]) {
-                            c++;
-                        }
-                    }
+
+            // Count elements smaller and larger to the right of nums[i]
+            for (int k = i + 1; k < n; k++) {
+                if (nums[k] < nums[i]) {
+                    rightSmaller++;
+                } else if (nums[k] > nums[i]) {
+                    rightLarger++;
                 }
             }
+
+            // Number of increasing teams with nums[i] as the middle element
+            count += leftSmaller * rightLarger;
+             
+            // Number of decreasing teams with nums[i] as the middle element
+            count += leftLarger * rightSmaller;
         }
-        
-        return c;
+
+        return count;
     }
 }
