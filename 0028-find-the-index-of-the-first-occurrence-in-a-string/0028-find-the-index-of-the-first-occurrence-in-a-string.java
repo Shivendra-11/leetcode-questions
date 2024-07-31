178352594ac5deb00380f660b71b36a2710faf4e
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-07-31 13:21:44 +0530

    Time: 0 ms (100%), Space: 41.3 MB (70.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,30 +1,31 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        // If needle is empty, return 0
-        if (needle.length() == 0) {
-            return 0;
-        }
+        int m=haystack.length();
+        int n=needle.length();
+        int first=0;
+        int second=0;
+        char str1 []=haystack.toCharArray();
+          char str2 []=needle.toCharArray();
+        
 
-        char[] str1 = haystack.toCharArray();
-        char[] str2 = needle.toCharArray();
-        int j = 0;
-
-        for (int i = 0; i < haystack.length(); i++) {
-            if (str1[i] == str2[j]) {
-                j++;
-            } else {
-                // Reset j and adjust i accordingly
-                i = i - j;
-                j = 0;
-            }
-
-            // If the entire needle has been found
-            if (j == str2.length) {
-                return i - j + 1;
+       for(int i=0;i<=m-n;i++){
+        first=i;
+        second=0;
+        while(second<n){
+            if(str1[first]!=str2[second]){
+                break; 
+            }else{
+                first++;
+                second++;
             }
         }
+        if(second==str2.length){
+            return first-second;
+        }
 
-        // Needle was not found
-        return -1;
+       }
+       return -1;
+
+        
     }
 }
