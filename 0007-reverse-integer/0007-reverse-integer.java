80d43a4a0e7f701220331b5ef6256cd2aa8026c8
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-09-22 01:25:25 +0530

    0007-REVERSE-INTEGER Stats: Time: 1 ms (84.39%), Space: 40.7 MB (74.51%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,11 +1,17 @@
 class Solution {
     public int reverse(int x) {
+
         long rem=0;
+        // int res=0;
         while(x!=0){
-            int mod=x %10;
-            rem=rem * 10 +mod;
-            x/=10;
+                 
+            int mod=x%10;
+            rem=rem*10+mod;
+            x=x/10;
+
         }
-        return (rem<Integer.MIN_VALUE || rem>Integer.MAX_VALUE)?0:(int) rem;
+        
+        return (rem<Integer.MIN_VALUE || rem>Integer.MAX_VALUE )?0: (int) (rem);
+        
     }
 }
--- README.md
+++ README.md
@@ -1397,6 +1397,7 @@ cyBFbmQtLT4=
 ## Math
 |  |
 | ------- |
+| [0007-reverse-integer](https://github.com/Shivendra-11/leetcode-questions/tree/master/0007-reverse-integer) |
 | [0013-roman-to-integer](https://github.com/Shivendra-11/leetcode-questions/tree/master/0013-roman-to-integer) |
 | [0050-powx-n](https://github.com/Shivendra-11/leetcode-questions/tree/master/0050-powx-n) |
 | [0069-sqrtx](https://github.com/Shivendra-11/leetcode-questions/tree/master/0069-sqrtx) |
