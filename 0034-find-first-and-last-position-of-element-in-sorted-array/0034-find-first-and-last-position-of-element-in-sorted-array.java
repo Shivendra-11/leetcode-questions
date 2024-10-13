315be87144b3e0c5ce0ed1ac2cf3d36ac20a3c93
Shivendra-11 <136923912+Shivendra-11@users.noreply.github.com>
2024-10-13 19:42:45 +0530

    0034-FIND-FIRST-AND-LAST-POSITION-OF-ELEMENT-IN-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 45.7 MB (83.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,43 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-int s=0;
-int e=nums.length-1;
-int first=-1;
-int last=-1;
+    public int[] searchRange(int[] arr, int target) {
+        
+        int s=0;
+        int e=arr.length-1;
+        int first=-1;
+        int second=-1;
+        int arr1[]=new int[2];
 
         while(s<=e){
-            int mid=(s+e)/2;
-            if(nums[mid]==target){
-                first=mid;
+            int mid=s+(e-s)/2;
+
+            if(arr[mid]==target){
+                first= mid;
                 e=mid-1;
-            }
-            else if(nums[mid]>target){
+            }else if(arr[mid]>target){
                 e=mid-1;
             }else{
                 s=mid+1;
             }
-            
         }
 
-            int low=0;
-            int high=nums.length-1;
-                
+        int low=0;
+        int high=arr.length-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                last=mid;
+            int mid=low+(high-low)/2;
+
+            if(arr[mid]==target){
+                second= mid;
                 low=mid+1;
-            }
-            else if(nums[mid]>target){
+            }else if(arr[mid]>target){
                 high=mid-1;
             }else{
                 low=mid+1;
             }
-            
         }
-int arr[]=new int[2];
-arr[0]=first;
-arr[1]=last;
-
-        
-        return arr;
+          for(int i=0;i<arr1.length;i++){
+            arr1[0]=first;
+            arr1[1]=second;
+          }
+return arr1;
     }
 }
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ made changes
 |  |
 | ------- |
 | [0015-3sum](https://github.com/Shivendra-11/leetcode-questions/tree/master/0015-3sum) |
+| [0034-find-first-and-last-position-of-element-in-sorted-array](https://github.com/Shivendra-11/leetcode-questions/tree/master/0034-find-first-and-last-position-of-element-in-sorted-array) |
 | [0035-search-insert-position](https://github.com/Shivendra-11/leetcode-questions/tree/master/0035-search-insert-position) |
 | [0042-trapping-rain-water](https://github.com/Shivendra-11/leetcode-questions/tree/master/0042-trapping-rain-water) |
 | [0053-maximum-subarray](https://github.com/Shivendra-11/leetcode-questions/tree/master/0053-maximum-subarray) |
@@ -22,6 +23,7 @@ made changes
 ## Binary Search
 |  |
 | ------- |
+| [0034-find-first-and-last-position-of-element-in-sorted-array](https://github.com/Shivendra-11/leetcode-questions/tree/master/0034-find-first-and-last-position-of-element-in-sorted-array) |
 | [0035-search-insert-position](https://github.com/Shivendra-11/leetcode-questions/tree/master/0035-search-insert-position) |
 | [0167-two-sum-ii-input-array-is-sorted](https://github.com/Shivendra-11/leetcode-questions/tree/master/0167-two-sum-ii-input-array-is-sorted) |
 ## Two Pointers
