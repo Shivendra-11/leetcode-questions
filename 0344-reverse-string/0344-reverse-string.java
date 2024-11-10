dd800ec6f96de7e9dc68ebc5d2ca768218d14e82
Shivendra-11 <shivendrakeshari581@gmail.com>
2024-11-11 00:21:15 +0530

    0344-REVERSE-STRING Stats: Time: 4 ms (7.24%), Space: 51.3 MB (5.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,16 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length;i++){
+            st.push(s[i]);
+        }
         int i=0;
-        int j=s.length-1;
-      while(i<j){
-        char temp=s[i];
-        s[i]=s[j];
-        s[j]=temp;
-        i++;
-        j--;
-
-
-      }
-
+        while(!st.empty()){
+            s[i]=st.pop();
+            i++;
+        }
+       
     }
 }
