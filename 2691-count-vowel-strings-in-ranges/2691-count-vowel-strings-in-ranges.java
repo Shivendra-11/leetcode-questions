991f0cf4e1913bd4dcdec0e81a8125226582611d
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-01-02 15:22:11 +0530

    2691-COUNT-VOWEL-STRINGS-IN-RANGES Stats: Time: 1989 ms (5.01%), Space: 88.6 MB (16.17%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,38 +1,33 @@
-
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
         int r = queries.length;
-        int[] res = new int[r];
-
-        // Set of vowels for quick lookup
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a');
-        vowels.add('e');
-        vowels.add('i');
-        vowels.add('o');
-        vowels.add('u');
-
-        // Preprocess the words to check if they start and end with vowels
-        int[] isVowelString = new int[words.length];
+        ArrayList<Character> li = new ArrayList<>();
+        li.add('a');
+        li.add('e');
+        li.add('i');
+        li.add('o');
+        li.add('u');
+        int isvowel[] = new int[words.length];
         for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (vowels.contains(word.charAt(0)) && vowels.contains(word.charAt(word.length() - 1))) {
-                isVowelString[i] = 1;
+
+            if (li.contains(words[i].charAt(0)) && (li.contains(words[i].charAt(words[i].length() - 1)))) {
+                isvowel[i] = 1;
             } else {
-                isVowelString[i] = 0;
+                isvowel[i] = 0;
             }
         }
 
-        // Process each query
+        // int p = 0;
+        int res[] = new int[r];
         for (int i = 0; i < r; i++) {
-            int count = 0;
+    int count=0;
             int start = queries[i][0];
             int end = queries[i][1];
-            // Count the words that are vowel strings in the range [start, end]
             for (int j = start; j <= end; j++) {
-                count += isVowelString[j];
+                count += isvowel[j];
             }
-            res[i] = count;
+
+           res[i]=count;
         }
 
         return res;
