2e2f511578d083045780f9bcd84a68e423ef117b
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-01-05 23:55:35 +0530

    3634-FIND-MIRROR-SCORE-OF-A-STRING Stats: Time: 35 ms (100%), Space: 46.3 MB (100%) - LeetHub

--- 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.java
+++ 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.java
@@ -1,23 +1,24 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
 class Solution {
     public long calculateScore(String s) {
         int n = s.length();
-        // Array of lists to store character positions
-        ArrayList<ArrayList<Integer>> occ = new ArrayList<>(26);
-        for (int i = 0; i < 26; i++) {
-            occ.add(new ArrayList<>());
-        }
+        // HashMap to store lists of character positions
+        HashMap<Character, ArrayList<Integer>> occ = new HashMap<>();
         
         long ans = 0;
         for (int i = 0; i < n; i++) {
-            char mirror = (char)('z' - s.charAt(i) + 'a'); // Calculate the mirror character
+            char mirror = (char) ('z' - s.charAt(i) + 'a'); // Calculate the mirror character
             
-            if (!occ.get(mirror - 'a').isEmpty()) {
+            if (occ.containsKey(mirror) && !occ.get(mirror).isEmpty()) {
                 // Get the last occurrence of the mirror character
-                int lastIndex = occ.get(mirror - 'a').remove(occ.get(mirror - 'a').size() - 1);
+                int lastIndex = occ.get(mirror).remove(occ.get(mirror).size() - 1);
                 ans += i - lastIndex; // Update the score
             } else {
                 // Add the current character's index to its list
-                occ.get(s.charAt(i) - 'a').add(i);
+                occ.putIfAbsent(s.charAt(i), new ArrayList<>());
+                occ.get(s.charAt(i)).add(i);
             }
         }
         
