c9b98b09eb36bd58880d51dee8a74945643a4cbe
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-01-18 15:53:55 +0530

    1039-FIND-THE-TOWN-JUDGE Stats: Time: 2 ms (99.97%), Space: 54.2 MB (33.14%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.java
+++ 1039-find-the-town-judge/1039-find-the-town-judge.java
@@ -1,11 +1,8 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        if(n==1) return 1;
-        else if (n==2 && trust.length==0  ) return -1;
+        // if(n==1) return 1;
+        // else if (n==2 && trust.length==0  ) return -1;
         int r = trust.length;
-       
-       
-
         int arr[] = new int[n + 1];
         for (int i = 0; i<r; i++) {
             arr[trust[i][0]]--;
