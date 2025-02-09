7adf3c52a8b7cf45e9abfbe9aebb2f3789300eb2
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-09 15:34:50 +0530

    2448-COUNT-NUMBER-OF-BAD-PAIRS Stats: Time: 42 ms (37.94%), Space: 58 MB (55.59%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.java
@@ -1,18 +1,15 @@
 class Solution {
     public long countBadPairs(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]-i;
-        }
-
+      
         HashMap<Integer,Integer> mp=new HashMap<>();
         long result=0;
         mp.put(nums[0],1);
         for(int i=1;i<nums.length;i++){
               int total=i;
-              int freq=mp.getOrDefault(nums[i],0);
+              int freq=mp.getOrDefault(nums[i]-i,0);
               int bp=total-freq;
               result+=bp;
-              mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+              mp.put(nums[i]-i,mp.getOrDefault(nums[i]-i,0)+1);
         }
         return result;
     }
