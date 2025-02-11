a9148c8b189f392843bfda44da8ee2500d22f349
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-11 22:46:22 +0530

    1627-LAST-MOMENT-BEFORE-ALL-ANTS-FALL-OUT-OF-A-PLANK Stats: Time: 4 ms (7.93%), Space: 45.2 MB (26.22%) - LeetHub

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.java
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/1627-last-moment-before-all-ants-fall-out-of-a-plank.java
@@ -1,15 +1,14 @@
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
-        
-    int res=0;
-        for(int i=0;i<left.length;i++){
-            res=Math.max(res,left[i]);
+       
+      int res = 0;
+        if (left.length > 0) {
+            Arrays.sort(left);
+            res = left[left.length - 1]; 
         }
-           for(int i=0;i<right.length;i++){
-            res=Math.max(res,n-right[i]);
-           }
-        
-        
+        for (int i = 0; i < right.length; i++) {
+            res = Math.max(res, n - right[i]);
+        }
         return res;
     }
 }
