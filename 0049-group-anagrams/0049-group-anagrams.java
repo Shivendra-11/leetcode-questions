c0ab08120242fba105145a8866504301bfc8febf
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-13 15:40:55 +0530

    0049-GROUP-ANAGRAMS Stats: Time: 6 ms (97.97%), Space: 47.7 MB (81.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,15 @@
 class Solution {
-    public String genrate(String s){
-        int arr[]=new int[26];
-
-        for(char ch:s.toCharArray()){
-            arr[ch-'a']++;
-        }
-
-             StringBuilder newS = new StringBuilder();
-
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>0){
-                newS.append(String.valueOf((char)(i+'a')).repeat(arr[i]));
-            }
-        }
-        return newS.toString();
-
-    }
     public List<List<String>> groupAnagrams(String[] strs) {
-                    
-       Map<String,List<String>> mp=new HashMap<>();
-
-      for(String s:strs){
-         String sorted=genrate(s);
-
-         if(!mp.containsKey(sorted)){
-            mp.put(sorted,new ArrayList<>());
-         }
-
-         mp.get(sorted).add(s);
-      }
-
+        HashMap<String,ArrayList<String>> mp=new HashMap<>();
+        for(String s:strs){
+            char arr[]=s.toCharArray();
+            Arrays.sort(arr);
+            String key=new String(arr);
+            if(!mp.containsKey(key)){
+                mp.put(key,new ArrayList<>());
+            }
+            mp.get(key).add(s);
+        }
         return new ArrayList<>(mp.values());
-
     }
 }
