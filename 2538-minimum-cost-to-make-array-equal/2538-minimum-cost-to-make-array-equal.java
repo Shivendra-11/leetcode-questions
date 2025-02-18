ec606650f4534c66fa4ce601e67d5ce5f728b8f1
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-19 01:10:49 +0530

    2538-MINIMUM-COST-TO-MAKE-ARRAY-EQUAL Stats: Time: 10 ms (95.68%), Space: 57.4 MB (6.17%) - LeetHub

--- 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.java
@@ -1,40 +1,38 @@
 class Solution {
-    public static long findc(int nums[],int cost[],long mid){
-        long res=0;
-        for(int i=0;i<nums.length;i++){
-            res+=Math.abs(mid-nums[i])*cost[i];
+    public static long findCost(int nums[], int cost[], long mid) {
+        long totalCost = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalCost += Math.abs(mid - nums[i]) * cost[i];
         }
-        return res;
+        return totalCost;
     }
-    public long minCost(int[] nums, int[] cost) {
-        long min = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < min) {
-                min = nums[i];
-            }
-        }
-        long max = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
-            }
-        }
-         
-        long l=min;
-        long h=max;
-        long res=Integer.MIN_VALUE;
-        while(l<=h){
-            long mid=l+(h-l)/2;
-            long c1=findc(nums,cost,mid);
-            long c2=findc(nums,cost,mid+1);
-            res=Math.min(c1,c2);
-            if(c1>c2){
-                l=mid+1;
-            }else{
-                h=mid-1;
-            }
 
+    public long minCost(int[] nums, int[] cost) {
+        long min = nums[0], max = nums[0];
+
+        
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
         }
-        return res==Integer.MIN_VALUE?0:res;
+
+        long l = min, h = max;
+        long res = Integer.MAX_VALUE;
+
+        while (l < h) {
+            long mid = l + (h - l) / 2;
+            long c1 = findCost(nums, cost, mid);
+            long c2 = findCost(nums, cost, mid + 1);
+
+            if (c1 < c2) { 
+                res = c1;  
+                h = mid;  
+            } else {
+                res = c2;
+                l = mid + 1;
+            }
+        }
+
+        return res==Integer.MAX_VALUE? 0:res;
     }
 }
