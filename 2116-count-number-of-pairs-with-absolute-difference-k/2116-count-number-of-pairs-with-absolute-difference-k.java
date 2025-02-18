609aa94c60ede27095a0af474e6d85dfd65428be
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-18 15:52:47 +0530

    2116-COUNT-NUMBER-OF-PAIRS-WITH-ABSOLUTE-DIFFERENCE-K Stats: Time: 4 ms (95.41%), Space: 44.1 MB (35.81%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,13 +1,16 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
-        Arrays.sort(nums);
+        HashMap<Integer,Integer> mp=new HashMap<>();
     int c=0;
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                int diff=Math.abs(nums[i]-nums[j]);
-                if(diff==k) c++;
-
-            }
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+       
+        for(int i=0;i<nums.length;i++){
+           int key=nums[i]-k;
+           if(mp.containsKey(key)){
+            c+=mp.get(key);
+           } 
         }
         return c;
     }
