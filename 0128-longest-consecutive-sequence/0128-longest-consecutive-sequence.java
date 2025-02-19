b30d5f970f53087f9bf847125b2b07048132fed9
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-19 10:53:54 +0530

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 16 ms (95.69%), Space: 56.8 MB (82.52%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,17 +7,18 @@ class Solution {
         if (nums.length == 0){
             return 0;
         }
-        int lastsmaller =nums[0];
+        // int lastsmaller =nums[0];
 
         for (int i = 1; i < nums.length; i++) {
+            if(nums[i]==nums[i-1]) continue;
 
-            if (nums[i] - 1 == lastsmaller) {
+            if (nums[i] - 1 == nums[i-1]) {
                 c++;
-
-            } else if (lastsmaller != nums[i]) {
+            } 
+            else{
                 c = 1;
             }
-            lastsmaller = nums[i];
+            // lastsmaller = nums[i];
 
             longest = Math.max(longest, c);
 
