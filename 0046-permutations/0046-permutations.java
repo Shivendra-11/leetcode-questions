80d152ff67f7e3cd17db47d593da4071031d7c71
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-20 21:58:20 +0530

    0046-PERMUTATIONS Stats: Time: 5 ms (2.89%), Space: 44.6 MB (46.24%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        backtrack(nums, new ArrayList<>(), visited, result);
-        return result;
+    public static void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
-
-    private void backtrack(int[] nums, List<Integer> tempList, boolean[] visited, List<List<Integer>> result) {
-        if (tempList.size() == nums.length) {
-            result.add(new ArrayList<>(tempList));
+    private void backtrack(int[] nums , List<List<Integer>> result,int idx) {
+     if (idx == nums.length) {
+            result.add(new ArrayList<>(Arrays.asList(Arrays.stream(nums).boxed().toArray(Integer[]::new))));
             return;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                tempList.add(nums[i]);
-
-                backtrack(nums, tempList, visited, result);
-
-                tempList.remove(tempList.size() - 1);
-                visited[i] = false;
-            }
+        
+        for (int i = idx; i < nums.length; i++) {
+            swap(nums,i,idx);
+            backtrack(nums,result,idx+1);
+             swap(nums,i,idx);
         }
     }
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums = {1, 2, 3};
-        System.out.println(sol.permute(nums));
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        // boolean[] visited = new boolean[nums.length];
+        backtrack(nums,result,0);
+        return result;
     }
 }
