4ae2374c7a1b951c24f2cbea70268828e0d12368
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-23 01:46:06 +0530

    1730-SPECIAL-ARRAY-WITH-X-ELEMENTS-GREATER-THAN-OR-EQUAL-X Stats: Time: 2 ms (52.27%), Space: 42 MB (6.82%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -4,23 +4,29 @@ class Solution {
     public static int finds(int nums[], int x) {
         int l = 0;
         int h = nums.length - 1;
-        while (l <= h) { 
+        while (l <= h) {
             int mid = l + (h - l) / 2;
             if (nums[mid] >= x) {
-                h = mid-1;
+                h = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
-        return nums.length - l; 
+        return nums.length - l;
     }
 
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
-        for (int i = 0; i <= nums.length; i++) { 
-            int c = finds(nums, i);
-            if (c == i) {
-                return i;
+        int l = 0;
+        int h = nums.length;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (finds(nums, mid) == mid) {
+                return mid;
+            } else if (finds(nums, mid) > mid) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
         return -1;
