f4316e9efbd9374f30310e330ed795db19c4b8f2
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-02-23 11:09:15 +0530

    3768-CHECK-IF-DIGITS-ARE-EQUAL-IN-STRING-AFTER-OPERATIONS-I Stats: Time: 8 ms (100%), Space: 44.8 MB (100%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+
+while(s.length()>2){
+    StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length()-1;i++){
+            int first=s.charAt(i)-'0';
+            int second=s.charAt(i+1)-'0';
+            int opr=(first+second) % 10;
+            sb.append(opr);
+
+        } 
+    s=sb.toString();
+    }
+return s.charAt(0)==s.charAt(1);
+    }
+}
