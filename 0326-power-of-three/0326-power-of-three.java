8c7dde7b60c872a30103541398b4d5d316f8cf64
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-05 00:24:58 +0530

    0326-POWER-OF-THREE Stats: Time: 8 ms (90.35%), Space: 44.1 MB (66.27%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,16 +1,9 @@
 class Solution {
-   
     public boolean isPowerOfThree(int n) {
-        if(n==0 || n<0 ) return false;
-        if(n==1) return true;
-       
-        while(n>0){
-            if(n%3!=0){
-                return false;
-            }
-            n=n/3;
-            if(n==1) return true;
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
         }
-        return false;
+        return n == 1;
     }
 }
