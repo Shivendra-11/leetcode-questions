3f4312ae279a7bf53ab78c5897699ab854557266
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-13 11:58:48 +0530

    3600-FIND-THE-K-TH-CHARACTER-IN-STRING-GAME-I Stats: Time: 2 ms (77.51%), Space: 41.7 MB (80%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
@@ -1,19 +1,19 @@
 class Solution {
-    public static void gen(StringBuilder s, int k) {
-        if (s.length() >= k) return; // Base case: Stop when length reaches k
-
-        int len = s.length(); // Get the current length before expanding
-        for (int i = 0; i < len; i++) {
-            char ch = (char) ('a' + (s.charAt(i) - 'a' + 1) % 26); // Generate next character
+    public static void gen(StringBuilder s,int k){
+        if(s.length()>=k){
+            return ;
+        }
+        int len=s.length();
+        for(int i=0;i<len;i++){
+            char ch=(char)('a'+(s.charAt(i)-'a'+1)%26);
             s.append(ch);
         }
-        
-        gen(s, k); // Recursive call to continue expanding
+         gen(s,k);
     }
-
     public char kthCharacter(int k) {
-        StringBuilder s = new StringBuilder("a"); // Start with 'a'
-        gen(s, k); // Recursively expand the sequence
-        return s.charAt(k - 1); // Return k-th character (1-based index)
+
+        StringBuilder s=new StringBuilder("a");
+        gen(s,k);
+        return s.charAt(k-1);
     }
 }
