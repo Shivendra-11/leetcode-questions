ce31448d2ad961932b8c8cfe7869a8520481b039
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-14 18:37:11 +0530

    1253-SORT-THE-MATRIX-DIAGONALLY Stats: Time: 13 ms (20.9%), Space: 44.9 MB (77.11%) - LeetHub

--- 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.java
@@ -1,28 +1,29 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-         Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
         int rows = mat.length;
         int cols = mat[0].length;
-
-        // Storing elements of each diagonal
+        
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                int key = i - j;
-                map.putIfAbsent(key, new ArrayList<>());
+                int key = i-j;
+                if(!map.containsKey(key)){
+                    map.put(key,new ArrayList<>());
+                }
                 map.get(key).add(mat[i][j]);
             }
         }
 
-        // Sorting the elements in each diagonal
+        
         for (ArrayList<Integer> list : map.values()) {
             Collections.sort(list);
         }
 
-        // Replacing elements in the matrix
+       
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 int key = i - j;
-                mat[i][j] = map.get(key).remove(0); // Remove from the start of the list
+                mat[i][j] = map.get(key).remove(0);
             }
         }
 
