af2fdbfe06af1135773517ec88e2e3599ab1a377
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-18 22:45:14 +0530

    2478-LONGEST-NICE-SUBARRAY Stats: Time: 4 ms (28.34%), Space: 56 MB (33.79%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.java
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.java
@@ -1,6 +1,7 @@
 class Solution {
     public int longestNiceSubarray(int[] nums) {
-        // int mask=0;
+        
+// first step is to compare the first and second element and then after we have to use or operation so that next element transfer all its 1 bit wich make the easy to comapre withi the next element..[3,8,48] ... in this first we perform the and operation and then after we use use or so that i can transfer all the 8 1 bit to the previous mask valyue and then check with the next element by doing and operation..
         int maxi=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             int mask=0;
