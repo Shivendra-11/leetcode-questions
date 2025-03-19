da20ca5a99798d7f9f62a528e426b70b8b784c23
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-19 21:58:20 +0530

    0491-NON-DECREASING-SUBSEQUENCES Stats: Time: 26 ms (9.99%), Space: 53.8 MB (11.97%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -6,32 +6,32 @@ class Solution {
         Set<List<Integer>> set = new HashSet<>();
         int n = nums.length;
 
-        // Generate all possible subsequences using nested loops
+        
         for (int i = 0; i < n; i++) {
             List<List<Integer>> newSubsequences = new ArrayList<>();
             
-            // Try to extend previous subsequences
+      
             for (List<Integer> seq : ans) {
-                if (nums[i] >= seq.get(seq.size() - 1)) { // Maintain non-decreasing order
+                if (nums[i] >= seq.get(seq.size() - 1)) { 
                     List<Integer> newSeq = new ArrayList<>(seq);
                     newSeq.add(nums[i]);
-                    if (!set.contains(newSeq)) {
+                    
                         set.add(newSeq);
                         newSubsequences.add(newSeq);
-                    }
+                    
                 }
             }
 
-            // Start a new subsequence with nums[i]
+            
             List<Integer> singleElementList = new ArrayList<>();
             singleElementList.add(nums[i]);
             newSubsequences.add(singleElementList);
 
-            // Add all newly created subsequences to ans
+            
             ans.addAll(newSubsequences);
         }
 
-        // Filter sequences with at least 2 elements
+     
         List<List<Integer>> result = new ArrayList<>();
         for (List<Integer> seq : set) {
             if (seq.size() >= 2) {
