98d213c56cd67aaad3ca0aada82e26e57f3b52ce
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-19 13:42:33 +0530

    3475-MINIMUM-OPERATIONS-TO-MAKE-BINARY-ARRAY-ELEMENTS-EQUAL-TO-ONE-I Stats: Time: 7 ms (66.53%), Space: 57.3 MB (61.4%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,18 +1,13 @@
 class Solution {
     public int minOperations(int[] nums) {
 int c=0;
-        for(int i=0;i<=nums.length-3;i++){
-          if(nums[i]==0){
-            int j=0;
+        for(int i=2;i<nums.length;i++){
+          if(nums[i-2]==0){
             c++;
-            while(j<3){
-                if(nums[j+i]==0){
-                    nums[j+i]=1;
-                }else{
-                    nums[j+i]=0;
-                }
-                j++;
-            }
+            nums[i-2]=nums[i-2]^1;
+            nums[i-1]=nums[i-1]^1;
+            nums[i]=nums[i]^1;
+            
           }
         }
         int sum=0;
