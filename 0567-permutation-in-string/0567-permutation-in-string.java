c014cbf0dd1498abfe170534d2fbd6de829ba112
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-03-25 19:57:56 +0530

    0567-PERMUTATION-IN-STRING Stats: Time: 6 ms (86.21%), Space: 42.6 MB (80.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,4 +1,13 @@
 class Solution {
+    public static boolean find(int arr[]){
+        for(int n:arr){
+            if(n!=0){
+                return false;
+            }
+          
+        }
+          return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
         int freq[]=new int[26];
         // int freq2[]=new int[26];
@@ -9,15 +18,14 @@ class Solution {
         int j=0;
         int count=s1.length();
         while(j<s2.length()){
-            if(freq[s2.charAt(j)-'a']>0){
-                count--;
-            }
+            // if(freq[s2.charAt(j)-'a']>0){
+            //     count--;
+            // }
             freq[s2.charAt(j)-'a']--;
             if(j-i+1==s1.length()){
-                  if(count==0) return true;
-                  if(freq[s2.charAt(i)-'a']>=0){
-                    count++;
-                  }
+                 if(find(freq)){
+                    return true;
+                 }
                   freq[s2.charAt(i)-'a']++;
                   i++;
             }
