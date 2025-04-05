4ed2c3d5c5b924044bcea38de091421495714074
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-04-06 01:51:19 +0530

    0070-CLIMBING-STAIRS Stats: Time: 0 ms (100%), Space: 40.4 MB (50%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -9,12 +9,11 @@ class Solution {
         if(dp[i]!=-1){
             return dp[i];
         }
-        int fibn=fib(i+1,dp,n)+ fib(i+2,dp,n);;
-         dp[i]=fibn;
-         return dp[i];
-
+        return dp[i]=fib(i+1,dp,n)+ fib(i+2,dp,n);
+        //  return dp[i]=fibn;
     }
     public int climbStairs(int n) {
+
        int dp[]=new int[n+1];
        Arrays.fill(dp,-1);
        return fib(0,dp,n);
