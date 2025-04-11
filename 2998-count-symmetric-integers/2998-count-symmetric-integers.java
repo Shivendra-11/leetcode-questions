3c2dfbf4a124df891bd2e4c0d11eb10e69e25dd6
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-04-11 12:15:40 +0530

    2998-COUNT-SYMMETRIC-INTEGERS Stats: Time: 35 ms (30.01%), Space: 44.6 MB (52.09%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,28 +1,29 @@
 class Solution {
-    public boolean check(int n){
-        int digit=0;
-        int sum=0;
-        // int p=n;
-        int sum2=0;
-        String str=Integer.toString(n);
-        int p=str.length();
-        String p1=str.substring(0,p/2);
-        String p2=str.substring(p/2);
-        for(int i=0;i<p1.length();i++){
-            sum+=p1.charAt(i)-'0';
+    public boolean check(int n) {
+        int digit = 0;
+        int sum = 0;
+        int sum2 = 0;
+        int p = n;
+        int q = n;
+        while (p > 0) {
+            sum += p % 10;
+            p = p / 10;
+            digit++;
         }
-         for(int i=0;i<p2.length();i++){
-            sum2+=p2.charAt(i)-'0';
+        for (int i = 0; i < digit / 2; i++) {
+            sum2 += q % 10;
+            q = q / 10;
         }
-        return sum==sum2?true:false;
-    } 
+        return (sum - sum2) == sum2 ? true : false;
+    }
+
     public int countSymmetricIntegers(int low, int high) {
-        int c=0;
-        
-        for(int i=low;i<=high;i++){
-            String q=Integer.toString(i);
-           if(q.length()%2!=0) {continue;}
-            else if((check(i))){
+        int c = 0;
+        for (int i = low; i <= high; i++) {
+            String q = Integer.toString(i);
+            if (q.length() % 2 != 0) {
+                continue;
+            } else if ((check(i))) {
                 c++;
             }
         }
