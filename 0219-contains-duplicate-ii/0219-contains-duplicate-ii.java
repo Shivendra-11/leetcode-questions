c84538eb1d73a0bfda6e757f48f03433a219eebc
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-07-13 21:59:37 +0530

    0219-CONTAINS-DUPLICATE-II Stats: Time: 40 ms (11.78%), Space: 56.6 MB (96.51%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,13 +1,37 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-      HashMap<Integer,Integer> mp=new HashMap<>();
+    public static boolean check(HashMap<Integer, Integer> mp) {
+        
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
 
-      for(int i=0;i<nums.length;i++){
-             if(mp.containsKey(nums[i]) && i-mp.get(nums[i])<=k){
+            if (value > 1) {
                 return true;
-             }
-             mp.put(nums[i],i);
-      }  
-      return false;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(k==0)return false;
+        int i = 0;
+        int j = 1;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(nums[0], 1);
+        int n = nums.length;
+        while (j < n) {
+            mp.put(nums[j], mp.getOrDefault(nums[j], 0) + 1);
+            if (mp.get(nums[j]) > 1)
+                return true;
+
+            if (j - i+1> k) {
+
+                mp.put(nums[i], mp.getOrDefault(nums[i], 0) - 1);
+                i++;
+
+            }
+            j++;
+        }
+        return false;
     }
 }
