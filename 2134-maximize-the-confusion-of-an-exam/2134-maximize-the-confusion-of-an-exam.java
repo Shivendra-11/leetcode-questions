84a78639cd1082427b7d4544f78ee7105fdb6a10
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-07-16 21:17:15 +0530

    2134-MAXIMIZE-THE-CONFUSION-OF-AN-EXAM Stats: Time: 62 ms (5.15%), Space: 45.2 MB (22.18%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
@@ -1,46 +1,21 @@
 class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
+        HashMap<Character,Integer>mp=new HashMap<>();
         int i=0;
         int j=0;
         int n=answerKey.length();
         int res=0;
-        int count=k;
-        //  case F->T
         while(j<n){
-            char ch=answerKey.charAt(j);
-            if(ch=='F'){
-                count--;
-            }
-            while(j<n && count<0){
-                if(answerKey.charAt(i)=='F'){
-                    count++;
-                }
-                i++;
-            }
-            res=Math.max(res,j-i+1);
-            j++;
-
-        }
-        // case 2:-"T"->"F":
-          i=0;
-          j=0;
-          count=k;
-        while(j<n){
-            char ch=answerKey.charAt(j);
-            if(ch=='T'){
-                count--;
-            }
-            while(j<n && count<0){
-                if(answerKey.charAt(i)=='T'){
-                    count++;
-                }
+          mp.put(answerKey.charAt(j),mp.getOrDefault(answerKey.charAt(j),0)+1);
+        
+            while(Math.min(mp.getOrDefault('T',0),mp.getOrDefault('F',0))>k){
+                mp.put(answerKey.charAt(i),mp.getOrDefault(answerKey.charAt(i),0)-1);
                 i++;
             }
             res=Math.max(res,j-i+1);
             j++;
         }
-
      return res;
-
+        
     }
 }
