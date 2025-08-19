2d00bf9e38a1d9b6047db926331e7fc3f275560e
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-08-19 23:46:35 +0530

    2432-NUMBER-OF-ZERO-FILLED-SUBARRAYS Stats: Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -1,15 +1,5 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        long c=0;
-        long res=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                c++;
-            }else{
-                c=0;
-            }
-            res+=c;
-        }
-        return res;
+        
     }
 }
