9b49cfad0e7ebf876861bc84cf2fccf15f6dc5a8
Shivendra-11 <shivendrakeshari581@gmail.com>
2025-10-31 10:32:00 +0530

    1013-FIBONACCI-NUMBER Stats: Time: 0 ms (100%), Space: 40.2 MB (92.17%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,17 @@
 class Solution {
-    public static int fib(int n,int dp[]){
-        if(n<=1){
+    public static int fibo(int n,int arr[]){
+        if(n==0||n==1){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        if(arr[n]!=-1){
+              return arr[n];
         }
-        return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+        arr[n] = fibo(n-1,arr)+fibo(n-2,arr);
+        return arr[n];
     }
     public int fib(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return fib(n,dp);
+        int arr[]=new int[31];
+        Arrays.fill(arr,-1);
+        return fibo(n,arr);
     }
 }
